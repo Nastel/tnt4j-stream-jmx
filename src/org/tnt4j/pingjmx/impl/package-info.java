bdaa9b3a8f4f2ba27c8d580b3109c4188d5de7d8
Albert <amavashev@nastel.com>
2015-01-28 20:48:31 -0500

    Added package-info
    Added package-info

---
+++ src/org/tnt4j/pingjmx/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package provides concrete implementations of <code>PingFactory</code> and
+ * <code>Pinger</code> interfaces for various runtime environments.
+ * 
+ * @author albert
+ * @version 1.0
+ */
+package org.tnt4j.pingjmx.impl;
