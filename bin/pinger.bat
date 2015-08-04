f85199ad9c0f80cf03204731cfd14cbfdbc16873
AlbertM <amavashev@nastel.com>
2015-08-04 11:32:11 -0400

    updated scripts, removed log4j dependency
    updated scripts, removed log4j dependency

--- bin/pinger.bat
+++ bin/pinger.bat
@@ -1,4 +1,4 @@
 set RUNDIR=%~p0
 set CLASSPATH="%RUNDIR%..\tnt4j-ping-jmx.jar;%RUNDIR%..\lib\tnt4j-api-final-all.jar"
-set TNT4JOPTS=-Dorg.tnt4j.pingagent.trace=true -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.on.exception=true -Dtnt4j.dump.provider.default=true -Dlog4j.configuration=file:"%RUNDIR%..\config\log4j.properties" -Dtnt4j.config="%RUNDIR%..\config\tnt4j.properties" -Dtnt4j.token.repository="%RUNDIR%..\config\tnt4j-tokens.properties"
+set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dorg.tnt4j.pingagent.trace=true -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.on.exception=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config="%RUNDIR%..\config\tnt4j.properties"
 java %TNT4JOPTS% -classpath %CLASSPATH% org.tnt4j.pingjmx.PingAgent "*:*" 10000 60000
