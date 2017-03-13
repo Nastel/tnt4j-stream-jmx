6b7b5cdc1eb7542d0be558b16a12701e100f4d1a
akausinis <andrius.kausinis@singleton-labs.lt>
2017-03-13 11:39:47 +0200

    run scripts update

--- bin/stream-jmx-attach.bat
+++ bin/stream-jmx-attach.bat
@@ -1,6 +1,7 @@
 setlocal
 set RUNDIR=%~p0
-set CLASSPATH="%RUNDIR%\..\tnt4j-stream-jmx*.jar;%RUNDIR%\..\lib\*"
+set TOOLS_PATH="%JAVA_HOME%\lib\tools.jar"
+set CLASSPATH="%RUNDIR%\..\tnt4j-stream-jmx*.jar;%RUNDIR%\..\lib\*;%TOOLS_PATH%"
 set TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=debug" "-Dcom.jkoolcloud.tnt4j.stream.jmx.agent.trace=true" "-Dtnt4j.dump.on.vm.shutdown=true" "-Dtnt4j.dump.on.exception=true" "-Dtnt4j.dump.provider.default=true" "-Dtnt4j.config=%RUNDIR%\..\config\tnt4j.properties"
 rem set TNT4JOPTS=%TNT4JOPTS% "-Dcom.jkoolcloud.tnt4j.stream.jmx.agent.validate.types=false"
-java %TNT4JOPTS% -classpath "%CLASSPATH%" com.jkoolcloud.tnt4j.stream.jmx.SamplingAgent "*:*" "" 10000 60000
+java %TNT4JOPTS% -classpath "%CLASSPATH%" com.jkoolcloud.tnt4j.stream.jmx.SamplingAgent -attach -vm:%* -ap:.\..\lib\tnt4j-stream-jmx-0.4.5.jar -ao:*:*!!10000
