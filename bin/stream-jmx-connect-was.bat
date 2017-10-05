78fa73c2bdba34ce881f65cdee2b07d343292395
akausinis <andrius.kausinis@singleton-labs.lt>
2017-10-05 10:41:53 +0300

    Updated WAS connect run scripts

--- bin/stream-jmx-connect-was.bat
+++ bin/stream-jmx-connect-was.bat
@@ -18,12 +18,12 @@ set LIBPATH=%RUNDIR%..\*;%RUNDIR%..\lib\*;%TOOLS_PATH%;%WAS_PATH%
 rem ---------------
 
 set TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=debug" "-Dcom.jkoolcloud.tnt4j.stream.jmx.agent.trace=true" "-Dtnt4j.dump.on.vm.shutdown=true" "-Dtnt4j.dump.on.exception=true" "-Dtnt4j.dump.provider.default=true" "-Dtnt4j.config=%RUNDIR%..\config\tnt4j.properties"
-set TNT4JOPTS=%TNT4JOPTS% "-Dcom.jkoolcloud.tnt4j.stream.jmx.agent.forceObjectName=true" "-Dcom.jkoolcloud.tnt4j.stream.jmx.sampler.factory=com.jkoolcloud.tnt4j.stream.jmx.impl.WASSamplerFactory" "-Djava.naming.provider.url=corbaname:iiop:localhost:2809"
+set TNT4JOPTS=%TNT4JOPTS% "-Dcom.jkoolcloud.tnt4j.stream.jmx.agent.forceObjectName=true" "-Dcom.jkoolcloud.tnt4j.stream.jmx.sampler.factory=com.jkoolcloud.tnt4j.stream.jmx.impl.WASSamplerFactory"
 
 set AGENT_OPTIONS=%2
 if "%AGENT_OPTIONS%"=="" set AGENT_OPTIONS=*:*!!10000
 
-set CONN_OPTIONS=-connect -vm:service:jmx:iiop://localhost:2809/jndi/JMXConnector
+set CONN_OPTIONS=-connect -vm:service:jmx:iiop://localhost:2809/jndi/JMXConnector -cp:java.naming.provider.url=corbaname:iiop:localhost:2809
 rem --- IF WAS and IBM JVM requires connection authentication or getting naming related exceptions ---
 rem set CONN_OPTIONS=%CONN_OPTIONS% -ul:Admin -up:admin -cp:java.naming.factory.initial=com.ibm.websphere.naming.WsnInitialContextFactory -cp:java.naming.factory.url.pkgs=com.ibm.ws.naming
 rem ---------------
--- bin/stream-jmx-connect-was.sh
+++ bin/stream-jmx-connect-was.sh
@@ -17,7 +17,7 @@ LIBPATH="$SCRIPTPATH/../*:$SCRIPTPATH/../lib/*:$TOOLS_PATH:$WAS_PATH"
 
 TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dcom.jkoolcloud.tnt4j.stream.jmx.agent.trace=true -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.on.exception=true -Dtnt4j.dump.provider.default=true"
 TNT4JOPTS="$TNT4JOPTS -Dtnt4j.config=$SCRIPTPATH/../config/tnt4j.properties"
-TNT4JOPTS="$TNT4JOPTS -Dcom.jkoolcloud.tnt4j.stream.jmx.agent.forceObjectName=true -Dcom.jkoolcloud.tnt4j.stream.jmx.sampler.factory=com.jkoolcloud.tnt4j.stream.jmx.impl.WASSamplerFactory -Djava.naming.provider.url=corbaname:iiop:localhost:2809"
+TNT4JOPTS="$TNT4JOPTS -Dcom.jkoolcloud.tnt4j.stream.jmx.agent.forceObjectName=true -Dcom.jkoolcloud.tnt4j.stream.jmx.sampler.factory=com.jkoolcloud.tnt4j.stream.jmx.impl.WASSamplerFactory"
 
 # NOTE! Double exclamation mark in bash has a special meaning (previous command)
 # to pass arguments correctly you need escape both. I.E. "stream-jmx-connect.sh tomcat *:*\!\!13000"
@@ -27,7 +27,7 @@ if ["$AGENT_OPTIONS" == ""]; then
     AGENT_OPTIONS="*:*!!10000"
 fi
 
-CONN_OPTIONS="-connect -vm:service:jmx:iiop://localhost:2809/jndi/JMXConnector"
+CONN_OPTIONS="-connect -vm:service:jmx:iiop://localhost:2809/jndi/JMXConnector -cp:java.naming.provider.url=corbaname:iiop:localhost:2809"
 ### --- IF WAS and IBM JVM requires connection authentication or getting naming related exceptions ---
 ### CONN_OPTIONS="$CONN_OPTIONS -ul:Admin -up:admin -cp:java.naming.factory.initial=com.ibm.websphere.naming.WsnInitialContextFactory -cp:java.naming.factory.url.pkgs=com.ibm.ws.naming"
 ### ---------------
