f2003fa55bdd6a3fcf2761fab19a581e9ee3eb0d
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2018-05-04 10:41:54 +0300

    field name fix.

--- tnt4j-stream-jmx-core/src/main/java/com/jkoolcloud/tnt4j/stream/jmx/format/FactNameValueFormatter.java
+++ tnt4j-stream-jmx-core/src/main/java/com/jkoolcloud/tnt4j/stream/jmx/format/FactNameValueFormatter.java
@@ -136,7 +136,7 @@ public class FactNameValueFormatter extends DefaultFormatter {
 		if (op.getCorrelator() != null) {
 			Set<String> cids = op.getCorrelator();
 			if (!cids.isEmpty()) {
-				selfSnapshot.add("corrid", cids);
+				selfSnapshot.add("corr-id", cids);
 			}
 		}
 		if (op.getUser() != null) {
