3214e80bdecdd584d58afa0a2bb6d90c5330fd97
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2023-11-06 17:49:03 +0200

    Moved source resolution to utilities class.

---
+++ tnt4j-stream-jmx-core/src/main/java/com/jkoolcloud/tnt4j/stream/jmx/source/JMXSourceUtils.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2014-2023 JKOOL, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jkoolcloud.tnt4j.stream.jmx.source;
+
+import java.util.Arrays;
+
+import com.jkoolcloud.tnt4j.config.DefaultConfigFactory;
+import com.jkoolcloud.tnt4j.config.TrackerConfig;
+import com.jkoolcloud.tnt4j.core.OpLevel;
+import com.jkoolcloud.tnt4j.sink.EventSink;
+import com.jkoolcloud.tnt4j.source.Source;
+import com.jkoolcloud.tnt4j.source.SourceFactory;
+import com.jkoolcloud.tnt4j.source.SourceType;
+import com.jkoolcloud.tnt4j.stream.jmx.SamplingAgentThread;
+
+/**
+ * JMX {@link Source} resolution utility methods.
+ * 
+ * @version $Revision: 1 $
+ */
+public class JMXSourceUtils {
+
+	/**
+	 * Returns filled-in source handle representing the user defined path.
+	 * 
+	 * @param cls
+	 *            class to resolve user defined source name
+	 * @param logger
+	 *            logger instance to log messages
+	 * @return source handle representing the user defined path, or root source if no user defined source path
+	 */
+	public static Source getSource(Class<?> cls, EventSink logger) {
+		return getSource(cls.getName(), logger);
+	}
+
+	/**
+	 * Returns filled-in source handle representing the user defined path.
+	 * 
+	 * @param className
+	 *            class name to resolve user defined source name
+	 * @param logger
+	 *            logger instance to log messages
+	 * @return source handle representing the user defined path, or root source if no user defined source path
+	 */
+	public static Source getSource(String className, EventSink logger) {
+		TrackerConfig config = DefaultConfigFactory.getInstance().getConfig(className);
+		SourceFactory instance = config.getSourceFactory();
+
+		String sourceDescriptor = null;
+		Thread thread = Thread.currentThread();
+		if (thread instanceof SamplingAgentThread) {
+			sourceDescriptor = ((SamplingAgentThread) thread).getVMSourceFQN();
+		}
+
+		if (sourceDescriptor == null) {
+			return instance.getRootSource();
+		}
+
+		Source source;
+
+		try {
+			source = instance.fromFQN(sourceDescriptor);
+		} catch (IllegalArgumentException e) {
+			// TODO placeholders doesn't fill right
+			logger.log(OpLevel.WARNING,
+					"JMXSourceUtils.getSource: source descriptor ''{1}'' does not match FQN pattern ''SourceType=SourceValue''. Erroneous source type. Valid types ''{0}''. Cause: {2}",
+					Arrays.toString(SourceType.values()), sourceDescriptor, e.getMessage());
+			throw new RuntimeException("Invalid Source configuration");
+		} catch (IndexOutOfBoundsException e) {
+			logger.log(OpLevel.WARNING,
+					"JMXSourceUtils.getSource: source descriptor ''{0}'' does not match FQN pattern ''SourceType=SourceValue''. Defaulting to ''SERVICE={0}''",
+					sourceDescriptor);
+			source = instance.newSource(sourceDescriptor, SourceType.SERVICE);
+		}
+
+		return source;
+	}
+}
